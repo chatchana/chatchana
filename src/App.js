7cb6543c16b934182971fdc3f1f88ac6f277d484
chatchana <chatchanak@gmail.com>
2019-04-21 00:08:36 +0700

    fourth commit

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
           </Row>
           <Row>
             <Col>Address</Col>
-            <Col>Bangkok, Thailand</Col>
+            <Col>Phayao, Thailand</Col>
           </Row>
         </Container>
 
