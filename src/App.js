3d5e08afa1bda580e94b51f5af8ab2d24cf346a9
chatchana <chatchanak@gmail.com>
2019-04-21 02:39:03 +0700

    fifth commit

--- src/App.js
+++ src/App.js
@@ -7,218 +7,281 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="app-left">
+        <div className="app-left" class="col-5">
           <img className="image"
             alt="profile"
             src="tam.jpg"
-            style={{ height: "200px"}} />
+            style={{ height: "200px" }} />
 
-          <h3>Chatchana Kruangsanuk</h3>
-          <h5>Java Programmer</h5>
-          
-        <Container>
-          <Row>
-            <Col>Email</Col>
-            <Col>chatchanak@gmail.com</Col>
-          </Row>
-          <Row>
-            <Col>Address</Col>
-            <Col>Phayao, Thailand</Col>
-          </Row>
-        </Container>
+          <p>
+            <h3>Chatchana Kruangsanuk</h3>
+            <h5>Java Programmer</h5>
+          </p>
 
-        <h3>Technical Skills</h3>
+          <p>
+          <div class="container">
+            <div class="row">
+              <div class="col">Email</div>
+              <div class="col">chatchanak@gmail.com</div>
+            </div>
+            <div class="row">
+              <div class="col">Address</div>
+              <div class="col">Phayao, Thailand</div>
+            </div>
+          </div>
+          </p>
+
+        <h3>Technical Skills</h3>        
         <p>
-          <Container>
-            <Row>
-              <Col>Java</Col>
-              <Col>
+          <div class="container">
+            <div class="row">
+              <div class="col">Java</div>
+              <div class="col">
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star unchecked"></span>
-              </Col>
-            </Row>
-            <Row>
-              <Col>Spring Boot</Col>
-              <Col>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">Spring Boot</div>
+              <div class="col">
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star unchecked"></span>
-              </Col>
-            </Row>
-            <Row>
-              <Col>Strut</Col>
-              <Col>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">Strut</div>
+              <div class="col">
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star unchecked"></span>
                 <span class="fa fa-star unchecked"></span>
-              </Col>
-            </Row>
-            <Row>
-              <Col>Hibernate</Col>
-              <Col>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">Hibernate</div>
+              <div class="col">
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star unchecked"></span>
                 <span class="fa fa-star unchecked"></span>
-              </Col>
-            </Row>
-            <Row>
-              <Col>Asp.net(C#)</Col>
-              <Col>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">Asp.net(C#)</div>
+              <div class="col">
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star unchecked"></span>
                 <span class="fa fa-star unchecked"></span>
-              </Col>
-            </Row>
-            <Row>
-              <Col>Javascript</Col>
-              <Col>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">Javascript</div>
+              <div class="col">
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star unchecked"></span>
                 <span class="fa fa-star unchecked"></span>
-              </Col>
-            </Row>
-            <Row>
-              <Col>HTML/CSS</Col>
-              <Col>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">HTML/CSS</div>
+              <div class="col">
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star unchecked"></span>
                 <span class="fa fa-star unchecked"></span>
                 <span class="fa fa-star unchecked"></span>
                 <span class="fa fa-star unchecked"></span>
-              </Col>
-            </Row>
-          </Container>
+              </div>
+            </div>
+            </div>
         </p>
         <h2>Language Skills</h2>
         <p>
-          <Container>
-            <Row>
-              <Col>Thai(Native)</Col>
-              <Col>
+          <div class="container">
+            <div class="row">
+              <div class="col">Thai(Native)</div>
+              <div class="col">
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
-              </Col>
-            </Row>
-            <Row>
-              <Col>English</Col>
-              <Col>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">English</div>
+              <div class="col">
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star unchecked"></span>
                 <span class="fa fa-star unchecked"></span>
                 <span class="fa fa-star unchecked"></span>
-              </Col>
-            </Row>
-          </Container>
-        </p>
-        <h3>Education</h3>
-        <p>
-          <Container>
-            <Row>
-              <Col>2007 - 2010</Col>
-              <Col>Bachelor of Science in Information Technology, Maejo University</Col>
-            </Row>
-          </Container>
+              </div>
+            </div>
+          </div>
         </p>
       </div>
 
-      <div className="app-right">
+      <div className="app-right" class="col-7">
+        <h3>Education</h3>
+        <p>
+          <div class="container">
+            <div class="row">
+              <div class="col">2007 - 2010</div>
+              <div class="col">Bachelor of Science in Information Technology, Maejo University</div>
+            </div>
+          </div>
+        </p>
         <h3>Experience</h3>
         <p>
-          <Container>
-            <Row className="headRow">
-              <Col><h5>Java Programmer, Krung Thai Bank</h5></Col>
-              <Col sm={{ size: 15, order: 2, offset: 0 }}>May 2018 - Present</Col>
-            </Row>
-            <Row>
-              <Col sm={{ size: 15, order: 2, offset: 1 }}><span class="dot"/>develop API with Java Spring Boot, Maven, Apache</Col>
-            </Row>
-            <Row>
-              <Col sm={{ size: 15, order: 2, offset: 1 }}><span class="dot"/>develop web application with Strut Framework, Oracle, JBoss</Col>
-            </Row>
-          </Container>
+          <div class="container">
+            <div class="row">
+              <div class="col">
+                <h5>Java Programmer, Krung Thai Bank</h5>
+              </div>
+              <div class="col" className="smCol">
+                May 2018 -  Present
+              </div>
+            </div>
+            <div class="row">
+              <div class="col"></div>
+            </div>
+            <div class="row">
+              <div class="col"><span class="dot" />
+                develop API with Java Spring Boot, Maven, Apache
+              </div>
+            </div>
+            <div class="row">
+              <div class="col"><span class="dot" />
+                develop web application with Strut Framework, Oracle, JBoss
+              </div>
+            </div>
+          </div>
         </p>
+
         <p>
-          <Container>
-          <Row className="headRow">
-              <Col><h5>Java Programmer, G-Able</h5></Col>
-              <Col sm={{ size: 15, order: 2, offset: 0 }}>June 2015 - April 2018</Col>
-            </Row>
-            <Row>
-              <Col sm={{ size: 15, order: 2, offset: 1 }}><span class="dot"/>develop web services with JAX-WS, Oracle, WebLogic</Col>
-            </Row>
-            <Row>
-              <Col sm={{ size: 15, order: 2, offset: 1 }}><span class="dot"/>develop web application with JSF Framework, Oracle</Col>
-            </Row>
-            <Row>
-              <Col sm={{ size: 15, order: 2, offset: 1 }}><span class="dot"/>gather the requirement, analyze and evaluate to come up with software requirement</Col>
-            </Row>
-            <Row>
-              <Col sm={{ size: 15, order: 2, offset: 1 }}><span class="dot"/>make program specification for the development team</Col>
-            </Row>
-            <Row>
-              <Col sm={{ size: 15, order: 2, offset: 1 }}><span class="dot"/>prepare test cycles, test data and expected results</Col>
-            </Row>
-          </Container>
+          <div class="container">
+            <div class="row">
+              <div class="col">
+                <h5>Java Programmer, G-Able</h5>
+              </div>
+              <div class="col" className="smCol">
+                June 2015 - April 2018
+              </div>
+            </div>
+            <div class="row">
+              <div class="col"></div>
+            </div>
+            <div class="row">
+              <div class="col"><span class="dot" />
+                develop web services with JAX-WS, Oracle, WebLogic
+              </div>
+            </div>
+            <div class="row">
+              <div class="col"><span class="dot" />
+                develop web application with JSF Framework, Oracle
+              </div>
+            </div>
+            <div class="row">
+              <div class="col"><span class="dot" />
+                gather the requirement, analyze and evaluate to come up with software requirement
+              </div>
+            </div>
+            <div class="row">
+              <div class="col"><span class="dot" />
+                make program specification for the development team
+              </div>
+            </div>
+            <div class="row">
+              <div class="col"><span class="dot" />
+                prepare test cycles, test data and expected results
+              </div>
+            </div>
+          </div>
         </p>
+
         <p>
-          <Container>
-          <Row className="headRow">
-              <Col><h5>Java Programmer(Outsource), Hitachi Consulting</h5></Col>
-              <Col sm={{ size: 15, order: 2, offset: 0 }}>June 2014 – June 2015</Col>
-            </Row>
-            <Row>
-              <Col sm={{ size: 15, order: 2, offset: 1 }}><span class="dot"/>develop web application with Strut Framework, Hibernate, Spring, Oracle, JBoss</Col>
-            </Row>
-            <Row>
-              <Col sm={{ size: 15, order: 2, offset: 1 }}><span class="dot"/>create batch job with Java</Col>
-            </Row>
-          </Container>
+          <div class="container">
+            <div class="row">
+              <div class="col">
+                <h5>Java Programmer(Outsource), Hitachi Consulting</h5>
+              </div>
+              <div class="col" className="smCol">
+                June 2014 – June 2015
+              </div>
+            </div>
+            <div class="row">
+              <div class="col"></div>
+            </div>
+            <div class="row">
+              <div class="col"><span class="dot" />
+                develop web application with Strut Framework, Hibernate, Spring, Oracle, JBoss
+              </div>
+            </div>
+            <div class="row">
+              <div class="col"><span class="dot" />
+                create batch job with Java
+              </div>
+            </div>
+          </div>
         </p>
+
         <p>
-          <Container>
-            <Row className="headRow">
-              <Col><h5>Java Programmer, Optimus Soft</h5></Col>
-              <Col sm={{ size: 15, order: 2, offset: 0 }}>Jan 2011 – May 2014</Col>
-            </Row>
-            <Row>
-              <Col sm={{ size: 15, order: 2, offset: 1 }}><span class="dot"/>develop web application with JSF Framework, Oracle, WebSphere, IBM RAD7</Col>
-            </Row>
-            <Row>
-              <Col sm={{ size: 15, order: 2, offset: 1 }}><span class="dot"/>create report with iReport</Col>
-            </Row>
-          </Container>
+          <div class="container">
+            <div class="row">
+              <div class="col">
+                <h5>Java Programmer, Optimus Soft</h5>
+              </div>
+              <div class="col" className="smCol">
+                Jan 2011 – May 2014
+              </div>
+            </div>
+            <div class="row">
+              <div class="col"></div>
+            </div>
+            <div class="row">
+              <div class="col"><span class="dot" />
+                develop web application with JSF Framework, Oracle, WebSphere, IBM RAD7
+              </div>
+            </div>
+            <div class="row">
+              <div class="col"><span class="dot" />
+                create report with iReport
+              </div>
+            </div>
+          </div>
         </p>
 
         <h3>Interests</h3>
         <p>
-          <Container>
-            <Row>
-              <Col sm={{ size: 15, order: 2, offset: 1 }}><span class="dot"/>reading books</Col>
-            </Row>
-            <Row>
-              <Col sm={{ size: 15, order: 2, offset: 1 }}><span class="dot"/>listening to music</Col>
-            </Row>
-            <Row>
-              <Col sm={{ size: 15, order: 2, offset: 1 }}><span class="dot"/>investment</Col>
-            </Row>
-          </Container>
+          <div class="container">
+            <div class="row">
+              <div class="col"><span class="dot" />
+                reading books
+              </div>
+            </div>
+            <div class="row">
+              <div class="col"><span class="dot" />
+                listening to music
+              </div>
+            </div>
+            <div class="row">
+              <div class="col"><span class="dot" />
+                investment
+              </div>
+            </div>
+          </div>
         </p>
       </div>
       </div >
